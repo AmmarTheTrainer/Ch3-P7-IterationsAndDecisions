78f9e1d2fc1194ab851d0c9c12146c129874a6c9
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-07 05:33:14 -0700

    commit 1
    take it please

---
+++ Ch3-P7-IterationsAndDecisions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3_P7_IterationsAndDecisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // chapter 3 - project 7 - Loops and decisions
+
+            Console.ReadLine();
+        }
+    }
+}
